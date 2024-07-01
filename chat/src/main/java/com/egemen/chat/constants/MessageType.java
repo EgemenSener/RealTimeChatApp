e6cd3278acd7b84300bb697b17f8669da23b98e4
Egemen Sener <egemensener98@gmail.com>
2024-07-01 22:02:25 +0300

    first commit!

---
+++ chat/src/main/java/com/egemen/chat/constants/MessageType.java
@@ -0,0 +1,7 @@
+package com.egemen.chat.constants;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
---
+++ chat/src/main/java/com/egemen/chat/entity/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.egemen.chat.entity;
+
+import com.egemen.chat.constants.MessageType;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private String content;
+    private String sender;
+    private MessageType type;
+}
